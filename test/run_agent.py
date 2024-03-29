df22ea88a4a7a47f654396e97281fcec9011e25f
Matoi647 <1524186933@qq.com>
2024-03-29 15:35:00 +0800

    add test folder

---
+++ test/run_agent.py
@@ -0,0 +1,54 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from argparse import ArgumentParser
+import pickle
+import cv2
+import numpy as np
+from pprint import pprint
+
+from minerl.herobraine.env_specs.human_survival_specs import HumanSurvival
+
+from agent import MineRLAgent, ENV_KWARGS
+
+height, width = 360, 640
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter('video.mp4', fourcc, 20.0, (width, height))
+video.set(cv2.VIDEOWRITER_PROP_QUALITY, 90)
+
+def main(model, weights):
+    env = HumanSurvival(**ENV_KWARGS).make()
+    print("---Loading model---")
+    agent_parameters = pickle.load(open(model, "rb"))
+    policy_kwargs = agent_parameters["model"]["args"]["net"]["args"]
+    pi_head_kwargs = agent_parameters["model"]["args"]["pi_head_opts"]
+    pi_head_kwargs["temperature"] = float(pi_head_kwargs["temperature"])
+    agent = MineRLAgent(env, policy_kwargs=policy_kwargs, pi_head_kwargs=pi_head_kwargs)
+    agent.load_weights(weights)
+
+    print("---Launching MineRL enviroment (be patient)---")
+    obs = env.reset()
+
+    # with open("obs.txt", "w") as f:
+    # while True:
+    for _ in range(1200):
+        minerl_action = agent.get_action(obs)
+        obs, reward, done, info = env.step(minerl_action)
+        # pprint(obs, stream=f)
+        env.render()
+        # break
+        img = obs['pov']
+        video.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    video.release()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser("Run pretrained models on MineRL environment")
+
+    parser.add_argument("--weights", type=str, required=True, help="Path to the '.weights' file to be loaded.")
+    parser.add_argument("--model", type=str, required=True, help="Path to the '.model' file to be loaded.")
+
+    args = parser.parse_args()
+
+    main(args.model, args.weights)
