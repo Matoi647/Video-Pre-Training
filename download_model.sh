ecb2c07d1a8a8d69481b53d9956393b3a5c7c393
Matoi647 <1524186933@qq.com>
2024-03-29 21:39:47 +0800

    add sh

---
+++ download_model.sh
@@ -0,0 +1,5 @@
+mkdir models/ weights/
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-1x.model -P models/
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/2x.model -P models/
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-3x.model -P models/
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/rl-from-early-game-2x.weights -P weights/
---
+++ test/test_minerl.py
@@ -0,0 +1,27 @@
+import gym
+import minerl
+import cv2
+
+# Uncomment to see more logs of the MineRL launch
+# import coloredlogs
+# coloredlogs.install(logging.DEBUG)
+
+height, width = 360, 640
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+video = cv2.VideoWriter('video.mp4', fourcc, 20.0, (width, height))
+video.set(cv2.VIDEOWRITER_PROP_QUALITY, 90)
+
+env = gym.make("MineRLBasaltBuildVillageHouse-v0")
+obs = env.reset()
+
+for i in range(600):
+    ac = env.action_space.noop()
+    # Spin around to see what is around us
+    ac["camera"] = [0, 3]
+    obs, reward, done, info = env.step(ac)
+    img = obs['pov']
+    video.write(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    print(i)
+    # env.render()
+video.release()
+env.close()
