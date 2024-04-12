fedff85b6691f0fa431e3dc5b3eb2b9e116e9774
Matoi647 <1524186933@qq.com>
2024-04-12 14:29:38 +0800

    download models

--- download_model.sh
+++ download_model.sh
@@ -3,3 +3,4 @@ wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-m
 wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/2x.model -P models/
 wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/foundation-model-3x.model -P models/
 wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/rl-from-early-game-2x.weights -P weights/
+wget https://openaipublic.blob.core.windows.net/minecraft-rl/models/rl-from-house-2x.weights -P weights/
